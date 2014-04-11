a28b2b0a43a1eb7963197a7f93b2caeca776536f
kutan007 <kutankhor@gmail.com>
2014-04-12 03:19:37 +0530

    my first commit

---
+++ src/org/roboage/iplocationfinder/IPLocationFinder.java
@@ -0,0 +1,25 @@
+package org.roboage.iplocationfinder;
+
+import java.util.Scanner;
+
+import net.webservicex.GeoIP;
+import net.webservicex.GeoIPService;
+import net.webservicex.GeoIPServiceSoap;
+
+public class IPLocationFinder {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner read = new Scanner(System.in);
+		System.out.println("Enter the IP Address: ");
+		String ipAddress = read.nextLine();
+		
+		GeoIPService objGeoIPService = new GeoIPService();
+		GeoIPServiceSoap objGeoIPServiceSoap = objGeoIPService.getGeoIPServiceSoap();
+		GeoIP objGeoIP = objGeoIPServiceSoap.getGeoIP(ipAddress);
+		System.out.println(objGeoIP.getCountryName());
+	}
+
+}
